99f433e6ddd210ff91da245ceee0859bc619224a
AMoreaux <moreaux.antoine@gmail.com>
2016-01-12 12:57:29 +0100

    meta

--- scripts/project.js
+++ scripts/project.js
@@ -3,20 +3,12 @@
  */
 
 var projectsBlock = document.getElementById('projects');
-var numberOfProjects = document.getElementsByTagName('section').length;
-var position = 0;
-var html = document.getElementsByTagName('html')[0];
 
 document.getElementById("displayProjects").addEventListener('click', function(){
   'use strict';
 
   projectsBlock.classList.add('projects-display');
-  html.style.overflowY = 'visible';
-
-  setTimeout(function(){
-    appears();
-  }, 400);
-
+  appears();
 });
 
 function appears(){
@@ -37,7 +29,6 @@ function disappears(){
 document.getElementById("back").addEventListener('click', function(){
   'use strict';
   projectsBlock.classList.remove('projects-display');
-  html.style.overflowY = 'hidden';
   disappears();
 
 });
